d432c9eea1d9ae57bf55cc874bd4a4d47c996dba
Akopyants <akopyants92@yandex.ru>
2023-03-26 23:34:27 +0300

    02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const VueCalculator = {
+    name: 'VueCalculator',
+
+    data() {
+        return {
+            firstNumber: 0,
+            secondNumber: 0,
+            operator: '',
+            result: 0
+        }
+    },
+
+    computed: {
+        calculate() {
+            const calc = (num1, num2, operator) => {
+                switch (operator) {
+                    case 'sum':
+                        return (num1 + num2);
+                    case 'subtract':
+                        return (num1 - num2);
+                    case 'multiply':
+                        return (num1 * num2);
+                    case 'divide':
+                        return (num1 / num2);
+                }
+            }
+
+            this.result = calc(this.firstNumber, this.secondNumber, this.operator);
+        }
+    }
+}
+
+
+const app = createApp(VueCalculator);
+const vm = app.mount('#app')
