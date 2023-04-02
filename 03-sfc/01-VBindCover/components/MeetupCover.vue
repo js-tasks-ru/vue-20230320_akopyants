5d6a5465d5a9f40354e7646c1d1beaff134d5d9c
Akopyants <akopyants92@yandex.ru>
2023-04-02 18:39:28 +0300

    «sfc»

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">
+      {{ title }}
+    </h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: {
+      type: String
+    },
+    image: {
+      type: String
+    }
+  },
+  computed: {
+    imageSrc() {
+      return `url(${this.image}`
+    }
+  }
 };
 </script>
 
@@ -15,8 +30,7 @@ export default {
   background-size: cover;
   background-position: center;
   background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  v-bind(imageSrc), var(--default-cover);
   display: flex;
   flex-direction: column;
   align-items: center;
