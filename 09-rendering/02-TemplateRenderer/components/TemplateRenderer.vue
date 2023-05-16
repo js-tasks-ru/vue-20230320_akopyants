26ff4033e278dbe0ab0cdd59b3b3f2ac4e84dc88
Akopyants <akopyants92@yandex.ru>
2023-05-16 23:05:50 +0300

    09-rendering

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,24 +1,36 @@
 <script>
-// import { compile } from 'vue';
+import { compile, h, defineComponent } from 'vue';
 
 export default {
   name: 'TemplateRenderer',
-
   props: {
     template: {
       type: String,
       required: true,
     },
-
     bindings: {
       type: Object,
       default: () => ({}),
     },
-
     components: {
       type: [Object, Array],
       default: () => [],
     },
   },
+  computed: {
+    component() {
+      return defineComponent({
+        components: this.components,
+        props: {
+          bindings: Object,
+        },
+        render: compile(this.template),
+      });
+    },
+  },
+  render() {
+    return h(this.component, { bindings: this.bindings });
+  },
 };
+
 </script>
